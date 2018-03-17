eac1c8aeb6a18304cab194fc6e5cb05efeec4236
uzairKandoi <36301070+uzairKandoi@users.noreply.github.com>
2018-03-17 13:00:17 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,142 +1,28 @@
-// Alerts Task
-
-// var x = prompt("Welcome");
-
-// var x = prompt("Please Enter a valid Password");
-
-// var x = prompt("Welcome to JS land \ Happy coding");
-
-// Variable for Strings task
-
-// var myName = "Uzair";
-
-// var message = "Hello World";
-// alert(message);
-
-// var x = "Ali Khan";
-// var y = "17 years old";
-// var z = "SSUET";
-// alert(x);
-// alert(y);
-// alert(z);
-
-// var a = "pizza \n pizz \n piz \n pi \n p";
-// var b = a.toLocaleUpperCase();
-// alert(b);
-
-// var a = prompt("number of children");
-// var b = prompt("partners name??");
-// var c = prompt("What work you do??")
-// alert("You" + "will" +  "be" + c + "inDubai" +
-//  "and" + "married to" + b + "with" + a + "kids");
-
-// var email = "abc@gmail.com"
-// alert("My" + "Email" + "address" + "is" + email);
-
-// var book = "A smarter way to learn Javascript";
-// alert(book);
-
-// var book = "A smarter way to learn Javascript";
-//  display.book;
-
-// Variables for numbers task
-// var age = 20;
-// alert(age);
-
-// var visits = 21;
-// alert("visits" + "" + visits + "" + "times");
-
-// var birthYear = 1998;
-// alert (birthyear);
-
-// var a = prompt("Your Name Plz");
-// var b = prompt("Your Product title");
-// var c = prompt("How many do you want to Buy")
-// alert(a + "ordered" + c + d +"on" + "XYZ" + "clothing"+ "store");
-
-//  Math Expressions
-
-// var a = 21;
-// var b = 20;
-// var c = a + b;
-// alert("sum of" + "" + a + "" + "and" + b + "" + "is" + c);
-
-
-// var a = 21;
-// var b = 20;
-// var c = a - b;
-// alert("difference of" + "" + a + "" + "and" + b + "" + "is" + c);
-
-
-// var a = 21;
-// var b = 20;
-// var c = a * b;
-// alert("multiplication" + "" + a + "" + "and" + b + "" + "is" + c);
-
-
-// var a = 105;
-// var b = 20;
-// var c = a % b;
-// alert("Modulus of" + "" + a + "" + "and" + b + "" + "is" + c);
-
-// var a = ("Value after variable declaration is??");
-// a = 26
-// document.write("initial value:" + a)
-// var b = a++ ;
-// document.write("value after increment is" + "" + b );
-// b = b + 7;
-// document.write("Value after addition is" + "" + b);
-// var c = b--;
-// document.write("value after decrement is" + "" + c);
-// var d = b % c;
-// document.write("the remainder is" + "" + d);
-
-
-// document.write("Multiplication table of 4")
-
-//  document.write("4" + "x" + 1 + "=" + (4*1));
-//  document.write("4" + "x" + 2 + "=" + (4*2));
-//  document.write("4" + "x" + 3 + "=" + (4*3));
-//  document.write("4" + "x" + 4 + "=" + (4*4));
-//  document.write("4" + "x" + 5 + "=" + (4*5));
-//  document.write("4" + "x" + 6 + "=" + (4*6));
-//  document.write("4" + "x" + 7 + "=" + (4*7));
-//  document.write("4" + "x" + 8 + "=" + (4*8));
-//  document.write("4" + "x" + 9 + "=" + (4*9));
-//  document.write("4" + "x" + 10 + "=" +(4*10));
-
-// var a = prompt("Item 1");
-// var b = prompt("item 2");
-// var c = prompt("How much item 1");
-// var d = prompt("How much item 2");
-// var e = prompt("shipping charges");
-// f = a + b + c + d + e;
-// document.write(a);
-// document.write(b);
-// document.write(c);
-// document.write(d);
-// document.write(e);
-// document.write("Total cost of your order is" + "" + f);
-
-// var a = prompt("Enter a number");
-// document.write("Multiplication table of" + a);
-
-//  document.write(a + "x" + 1 + "=" + (a*1));
-//  document.write(a + "x" + 2 + "=" + (a*2));
-//  document.write(a + "x" + 3 + "=" + (a*3));
-//  document.write(a + "x" + 4 + "=" + (a*4));
-//  document.write(a + "x" + 5 + "=" + (a*5));
-//  document.write(a + "x" + 6 + "=" + (a*6));
-//  document.write(a + "x" + 7 + "=" + (a*7));
-//  document.write(a + "x" + 8 + "=" + (a*8));
-//  document.write(a + "x" + 9 + "=" + (a*9));
-//  document.write(a + "x" + 10 + "=" +(a*10));
-
-// var a = prompt("Enter you gender");
-// if (a = "male"); {
-//     alert("Good Morning Sir");
-//     elseif(a = "female"); {
-//     alert("Good Morning Maam");
-//     }
-// } 
-
+function add() {
+     var val = document.getElementById("val");
+     var list = document.getElementById("list");
+     var li = document.createElement("LI");
+     var btn = document.createElement("BUTTON");
+     var btnText = document.createTextNode("Hogaya");
+     btn.appendChild(btnText);
+     btn.onclick = function() {
+         var li = this.parentNode;
+         var ul = li.parentNode;
+         ul.removeChild(li)
+     }
+    var text = document.createTextNode(val.value);
+    li.appendChild(text)
+    li.appendChild(btn)
+    list.appendChild(li)
+    val.value = " ";
+}
+    function deleteAll() {
+        var x = prompt ("Karliya Sab??")
+        if (x === "yes") {
+        document.getElementById("list").innerText = " ";
+        }
+        else if(x === "no") {
+            (alert("Phle Kar to Ley!!"));
+         }
+        
+    }
